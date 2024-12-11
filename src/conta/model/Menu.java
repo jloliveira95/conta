f7561f98f21d17f953de84fb624ccdf9c5387ca0
jloliveira95 <jl.oliveira95@gmail.com>
2024-12-11 14:05:04 -0300

    Commit inicial do projeto conta bnacária

--- src/conta/model/Menu.java
+++ src/conta/model/Menu.java
@@ -1,13 +1,15 @@
 
 package conta.model;
 
-import conta.model;
 public class Menu {
     public static void main(String[] args) {
+        
+        Conta c1 = new Conta(123456,0001,013,"joaozão", 500);
+        
+    }
         
     }
     
     // Instanciamento | Classe -> Objeto Utilizavel
-    conta.Conta c1 = new conta.Conta(123456,0001,449,"joao",500f);
-    c1.setSaldo(5000f);
-}
+    
+
